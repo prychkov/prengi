d83b56157e0affccebf61badc0762e3dd51f70aa
Pavel Rychkov <rychkovpavel@yandex.kz>
2020-06-02 07:57:22 +0600

    Add header and first screen

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.promo__carousel').slick({
+        dots: true,
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+    });
+});
