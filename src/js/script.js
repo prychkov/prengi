f08fa5142488b221353fe9130dd560b64d33ea5a
Pavel Rychkov <rychkovpavel@yandex.kz>
2020-06-03 12:15:09 +0600

    Add section advantages

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
         dots: true,
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/first_slider/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/first_slider/arrow_right.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/first_slider/arrow_right.png"></button>'
+    });
+    $('.solution__carousel').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/solution/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/solution/arrow_right.png"></button>'
     });
 });
