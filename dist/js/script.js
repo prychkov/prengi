fab595d92ab26b208c25a20b55b03b7ed14349a9
Pavel Rychkov <rychkovpavel@yandex.kz>
2020-06-03 06:23:18 +0600

    Add section with prengi

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.promo__carousel').slick({
         dots: true,
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/first_slider/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/first_slider/arrow_right.png"></button>',
     });
 });
