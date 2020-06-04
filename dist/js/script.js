11eb65444cabeb922a6957d069ae295e3e44b665
Pavel Rychkov <rychkovpavel@yandex.kz>
2020-06-04 17:01:38 +0600

    Added responsive markup for intervals from 1 to 10

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,27 @@ $(document).ready(function(){
         dots: true,
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/first_slider/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/first_slider/arrow_right.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/first_slider/arrow_right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
     $('.solution__carousel').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/solution/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/solution/arrow_right.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/solution/arrow_right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 });
